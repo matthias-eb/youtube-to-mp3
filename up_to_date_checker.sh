68a8568758be6cd663ea3beadd0dbd61fd75dd08
meberlei <matthiaseberlein.me@gmail.com>
2020-09-03 18:12:53 +0200

    Changed logging system. Messages are saved as commands and executed later. Unnecessary ouput redirected to /dev/null. Script Stage shoud be much clearer now.

--- up_to_date_checker.sh
+++ up_to_date_checker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ## This script is for keeping the AUR package youtube-to-mp3 up to date with the md5sums.
-## It automates the packaging, pushing and is keeping the files koherent. It updates the Checksums once per date.
+## It automates the packaging, pushing and is keeping the files coherent. It updates the Checksums once per date.
 ##
 ## Exit states:
 ##	0: Everything went according to plan.
@@ -11,14 +11,39 @@
 ## The md5 checksum Files are located in the update_script branch.
 ## PKGBUILD and .SRCINFO are located in the master branch. This Branch is pushed to the aur and origin remote master branches.
 
+
+
+RED="$(tput setaf 1)" # Red Foreground Color
+GREEN="$(tput setaf 2)" # Green Foreground Color
+BG_WHITE="$(tput setab 7)" # White Background
+BOLD="$(tput bold)" # Bold font
+RS="$(tput sgr 0)" # Reset Color and font
+
+# Output Messages
+OPT_PUSH_AUR="echo ${BOLD}==> Pushing to AUR remote repository...${RS}"
+OPT_PUSH_MASTER="echo ${BOLD}==> Pushing to origin master branch...\t${RS}"
+OPT_PUSH_UPDATE_SCRIPT="echo ${BOLD}==> Pushing to origin update_script branch...\t${RS}"
+OPT_NEW_COMMIT="echo ${BOLD}${GREEN}==> New Commit for branch $(git branch --show-current) created. Commit Message: $(git log -1 --format=format:%s)${RS}"
+OPT_RESET="echo ${BOLD}${RED}${BG_WHITE}==> Removing commits:${RS}${BG_WHITE}" # Intro for the Resets. Keeps the White background to clarify what belongs to the Reset. Needs to be Reset afterwards
+OPT_DATE_UPDATE_32="echo ${BOLD}==> 32 Checksum OK, refreshing date...${RS}"
+OPT_DATE_UPDATE_64="echo ${BOLD}==> 64 Checksum OK, refreshing date...${RS}"
+OPT_32_OK="echo ${BOLD}==> 32 Checksum OK${RS}"
+OPT_64_OK="echo ${BOLD}==> 64 Checksum OK${RS}"
+OPT_DONE="echo ${BOLD}==> ...done${RS}"
+OPT_REM_FILES="echo ${BOLD}==> Removing created files.${RS}"
+OPT_MD5_32_CHANGED="echo ${BOLD}${RED}==> Md5sum for Architecture i386 changed! Updating.${RS}"
+OPT_MD5_64_CHANGED="echo ${BOLD}${RED}==> Md5sum for Architecture x86_64 changed! Updating.${RS}"
+OPT_ERR_DOWNLOAD="echo ${BOLD}${RED}Downloading .deb File failed. Please check the error above. Maybe an internet connection is not established?${RS}"
+OPT_ERR_BUILD_FAIL="echo ${BOLD}${RED}Building the package failed. Please check above. Reverting commits..."
+OPT_ERR_PUSH="echo ${BOLD}${RED}Pushing failed. Please check above for errors.${RS}"
+OPT_EXIT_SCRIPT="echo ${BOLD}Finished.${RS}"
+
 MSG_32_SUM_CHANGED="32 Bit md5sum changed for PKGBUILD"
 MSG_32_DATE_CHANGED="Date of the 32 Bit md5sum file changed"
 MSG_64_SUM_CHANGED="64 Bit md5sum changed for PKGBUILD"
 MSG_64_DATE_CHANGED="Date of the 64 Bit md5sum file changed"
 MSG_DATE_ADDED="Date added for checksum File"
 
-RED='\033[0;31m'
-NC='\033[0m' # No Color
 changes_made=false
 skip=false
 md5_changes=false
@@ -27,22 +52,27 @@ md5_changes=false
 # This means that this script will run again after fixing the problem that led to reverting the commits since the date commit is thrown away as well.
 # This function prevents pushing wrong sha256 sums to the git repository.
 function revertCommits() {
-	## Revert committed changes
+	# Start white Background in console and Write first output
+	$OPT_RESET
+
 	last_commit=$(git log -1 --format=format:%s)
 	commit_date=$(git log -1 --date=short --format=format:%cd)
-	# Remove tha last commit as long as it has the current date and 
+	# Remove the last commit as long as it has the current date and the commit Message matches one of the specified commit messages.
 	while [ \( "$last_commit" = "$MSG_32_SUM_CHANGED" -o "$last_commit" = "$MSG_64_SUM_CHANGED" -o "$last_commit" = "$MSG_DATE_ADDED" \) -a \( "$commit_date" = "$(date --rfc-3339=date)" \) ]; do
-		echo -e "${RED}Removing following commit:${NC}" 
+		echo "\n------------------------------\n"
 		git log -1
 		git reset --hard HEAD~1
 		
 		last_commit=$(git log -1 --format=format:%s)
 		commit_date=$(git log -1 --date=short --format=format:%cd)
 	done
+	echo "$RS"
 }
 
 function removeFiles() {
 	## Try to delete those Files while ignoring the output
+	$OPT_REM_FILES
+
 	rm youtube-to-mp3_i386.deb > /dev/null 2>&1
 	rm youtube-to-mp3_x86_64.deb > /dev/null 2>&1
 	rm *.pkg.tar.xz > /dev/null 2>&1
@@ -54,7 +84,7 @@ function getSources_i386() {
 	# Download i386 source
 	wget --no-check-certificate -O "youtube-to-mp3_i386.deb" "https://www.mediahuman.com/de/download/YouTubeToMP3.i386.deb" # Download 32 Bit File
 	if [ $? -ne 0 ]; then
-		echo -e "${RED}Downloading .deb File failed. Please check the error above. Maybe an internet connection is not established?${NC}"
+		$OPT_ERR_DOWNLOAD
 		removeFiles
 		exit -1
 	fi
@@ -64,7 +94,7 @@ function getSources_x86_64() {
 	# Download sources for x86_64 Architecture
 	wget --no-check-certificate -O "youtube-to-mp3_x86_64.deb" "https://www.mediahuman.com/de/download/YouTubeToMP3.amd64.deb" # Download 64 Bit File
 	if [ $? -ne 0 ]; then
-		echo -e "${RED}Downloading .deb File failed. Please check the error above. Maybe an internet connection is not established?${NC}"
+		$OPT_ERR_DOWNLOAD
 		removeFiles
 		exit -1
 	fi
@@ -76,19 +106,19 @@ function update_md5_i386() {
 	if [ $? -eq 0 ]; then
 		# The checksum matches
 		# Change to the correct branch if necessary
-		if [ "$(git branch --show-current)" != "update_script" ]; then
 			git checkout update_script
 		fi
 		# Check, if the date still matches
 		if [ "$(date --rfc-3339=date)" != "$(sed '2q;d' md5sum_i386)" ]; then
-			echo "Changing date for 32 Bit md5sum file"
+			$OPT_DATE_UPDATE_32
 			# If the date does'nt match, replace the second line in the file with the new md5sum
 			sed -i "2s/.*/$(date --rfc-3339=date)/" md5sum_i386
-			git add md5sum_i386
-			git commit -m "$MSG_32_DATE_CHANGED"
+			git add md5sum_i386 > /dev/null
+			git commit -m "$MSG_32_DATE_CHANGED" > /dev/null
+			$OPT_NEW_COMMIT
 			md5_changes=true
 		else
-			echo "File 'youtube-to-mp3_i386.deb' OK."
+			$OPT_32_OK
 		fi
 	else
 		# The md5sum changed. We need to replace it in both the PKGBUILD and the md5sum file.
@@ -96,22 +126,25 @@ function update_md5_i386() {
 		if [ "$(git branch --show-current)" != "master" ]; then
 			git checkout master
 		fi
-		echo -e "${RED}Md5sum for Architecture i386 changed! Updating.${NC}"
+		$OPT_MD5_32_CHANGED
 		# Get the md5sum from the deb File
 		md5_i386=$(md5sum "youtube-to-mp3_i386.deb" | cut -d ' ' -f 1)
 		# Replace the first line in PKGBUILD starting with md5sums_i386=( with the same string and the added new md5sum as well as a ")"
 		sed -i "s/md5sums_i386=(.\+/md5sums_i386=(\"${md5_i386}\")/" PKGBUILD
 		# Set the flag to push the master branch of the aur and origin 
 		changes_made=true
-		git add PKGBUILD
-		git commit -m "$MSG_32_SUM_CHANGED"
+		git add PKGBUILD > /dev/null
+		git commit -m "$MSG_32_SUM_CHANGED" > /dev/null
 
 		# Now, rewrite the md5sum file and commit to the update_script branch as well 
 		git checkout update_script
 		md5sum "youtube-to-mp3_i386.deb" > md5sum_i386
 		date --rfc-3339=date >> md5sum_i386
-		git add md5sum_i386
-		git commit -m "$MSG_32_SUM_CHANGED"
+		git add md5sum_i386 > /dev/null
+		git commit -m "$MSG_32_SUM_CHANGED" > /dev/null
+
+		$OPT_NEW_COMMIT
+
 		# Set the flag to push to the update_script branch
 		md5_changes=true
 	fi
@@ -127,13 +160,15 @@ function update_md5_x86_64() {
 		fi
 		# Check, if the date still matches
 		if [ "$(date --rfc-3339=date)" != "$(sed '2q;d' md5sum_x86_64)" ]; then
-			echo "Changing date for 64 Bit md5sum file"
+			$OPT_DATE_UPDATE_64
+
 			sed -i "2s/.*/$(date --rfc-3339=date)/" md5sum_x86_64
-			git add md5sum_x86_64
-			git commit -m "$MSG_64_DATE_CHANGED"
+			git add md5sum_x86_64 > /dev/null
+			git commit -m "$MSG_64_DATE_CHANGED" > /dev/null
+			$OPT_NEW_COMMIT
 			md5_changes=true
 		else
-			echo "File 'youtube-to-mp3_x86_64.deb' OK."
+			$OPT_64_OK
 		fi
 	else
 		# The md5sum changed. We need to replace it in both the PKGBUILD and the md5sum file.
@@ -141,22 +176,25 @@ function update_md5_x86_64() {
 		if [ "$(git branch --show-current)" != "master" ]; then
 			git checkout master
 		fi
-		echo -e "${RED}Md5sum for Architecture x86_64 changed! Updating.${NC}"
+		$OPT_MD5_64_CHANGED
 		# Get the md5sum from the deb File
 		md5_x86_64=$(md5sum "youtube-to-mp3_x86_64.deb" | cut -d ' ' -f 1)
 		# Replace the first line in PKGBUILD starting with 'md5sums_x86_64=(' with that same string and the added new md5sum as well as a ')'"
 		sed -i "s/md5sums_x86_64=(.\+/md5sums_x86_64=(\"${md5_x86_64}\")/" PKGBUILD
 		# Set the flag to push the master branch of the aur and origin
 		changes_made=true
-		git add PKGBUILD
-		git commit -m "$MSG_64_SUM_CHANGED"
+		git add PKGBUILD > /dev/null
+		git commit -m "$MSG_64_SUM_CHANGED" > /dev/null
+
+		$OPT_NEW_COMMIT
 
 		# Now, rewrite the md5sum file and commit to the update_script branch as well 
 		git checkout update_script
 		md5sum "youtube-to-mp3_x86_64.deb" > md5sum_x86_64
 		date --rfc-3339=date >> md5sum_x86_64
-		git add md5sum_x86_64
-		git commit -m "$MSG_64_SUM_CHANGED"
+		git add md5sum_x86_64 > /dev/null
+		git commit -m "$MSG_64_SUM_CHANGED" > /dev/null
+		$OPT_NEW_COMMIT
 		# Set the flag to push to the update_script branch
 		md5_changes=true
 	fi
@@ -165,22 +203,20 @@ function update_md5_x86_64() {
 function buildPackage() {
 	makepkg -cf
 	if [ $? -ne 0 ]; then
-		echo -e "${RED}Building the package failed. Please check above. Reverting commits..."
+		$OPT_ERR_BUILD_FAIL
 		revertCommits
-		echo -e "...done. Removing created files..."
 		removeFiles
-		echo -e "...done. Exiting.${NC}"
 		exit -2
 	fi
 }
 
 
 if [ -f "youtube-to-mp3_i386.deb" ]; then
-	echo -e "${RED}Deleting existing 'youtube-to-mp3_i386.deb' file..${NC}"
+	echo -e "${RED}Deleting existing 'youtube-to-mp3_i386.deb' file..${RS}"
 	rm "youtube-to-mp3_i386.deb"
 fi
 if [ -f "youtube-to-mp3_x86_64.deb" ]; then
-	echo -e "${RED}Deleting existing 'youtube-to-mp3_x86_64.deb' file..${NC}"
+	echo -e "${RED}Deleting existing 'youtube-to-mp3_x86_64.deb' file..${RS}"
 	rm "youtube-to-mp3_x86_64.deb"
 fi
 
@@ -192,10 +228,15 @@ if [ -f md5sum_i386 ]; then
 		md5_date_i386="$(sed -n '2,2p' md5sum_i386)"
 		if [ "$md5_date_i386" == "$(date --rfc-3339=date)" ]; then
 			# If the date is the same, the file does not need to be updated. Proceed with the next one
-			echo "MD5Sum for i386 Architecture is up to date."
+			$OPT_32_OK
 			skip=true
 		fi
 	else
+		# If the date line does not exist, add and commit it.
+		date --rfc-3339=date >> md5sum_i386
+		git add md5sum_i386 > /dev/null
+		git commit -m "$MSG_DATE_ADDED" > /dev/null
+		$OPT_NEW_COMMIT
 		# Set md5changes to true to absolutely push the md5sum Files, even if they are not with new checksums.
 		md5_changes=true
 	fi
@@ -224,13 +265,15 @@ if [ -f md5sum_x86_64 ]; then
 		md5_date_x86_64="$(sed -n '2,2p' md5sum_x86_64)"
 		if [ "$md5_date_x86_64" == "$(date --rfc-3339=date)" ]; then
 			# This md5sum is up to date, the date is the same.
-			echo "MD5Sum for x86_64 Architecture is up to date."
+			$OPT_64_OK
 			skip=true
 		fi
 	else
+		# If the date line does not exist, add and commit it.
 		date --rfc-3339=date >> md5sum_x86_64
-		git add md5sum_x86_64
-		git commit -m "$MSG_DATE_ADDED"
+		git add md5sum_x86_64 > /dev/null
+		git commit -m "$MSG_DATE_ADDED" > /dev/null
+		$OPT_NEW_COMMIT
 		# Set md5changes to true to absolutely push the md5sum Files, even if they are not with new checksums.
 		md5_changes=true
 	fi
@@ -277,16 +320,18 @@ if [ $changes_made == "true" ]; then
 	# Update .SRCINFO
 	makepkg --printsrcinfo > .SRCINFO
 	git add PKGBUILD .SRCINFO
-	git commit -m ".SRCINFO regenerated, updated Package Version to ${pkgverline#*=} and Package Release Nr. to $pkgrel"
+	git commit -m ".SRCINFO regenerated, updated Package Version to ${pkgverline#*=} and Package Release Nr. to $pkgrel" > /dev/null
+	$OPT_NEW_COMMIT
 
 	# Update repository
-	git status
+	git status -sb
 	echo "Do you want to publish the updated PKGBUILD and .SRCINFO to the AUR?"
 	answer=""
 	while [ "$answer" != "n" ] && [ "$answer" != "y" ] && [ "$answer" != "yes" ] && [ "$answer" != "no" ]; do
 		read answer
 		answer="${answer,,}"
 		if [ "$answer" = "yes" ] || [ "$answer" = "y" ]; then
+			$OPT_PUSH_AUR
 			git push aur
 			if [ $? -ne 0 ]; then
 				echo "Pushing failed. Please check for errors."
@@ -298,19 +343,23 @@ if [ $changes_made == "true" ]; then
 			echo "Please Write one of the following and press enter: yes/no/y/n"
 		fi
 	done
-	echo "Pushing PKGBUILD and .SRCINFO to home repository.."
+	$OPT_PUSH_MASTER
 	git push origin master
 	if [ $? -ne 0 ]; then
-		echo -e "${RED}Pushing failed. Please check for errors.${NC}"
+		$OPT_ERR_PUSH
 		exit -3
 	fi
 fi
 
 if [ "$md5_changes" == "true" ]; then
 	# Now pushing the md5sums
-	echo "Pushing the updated checksums to the home repository"
+	$OPT_PUSH_UPDATE_SCRIPT
 	git checkout update_script
 	git push
-	echo "Push successful."
+	if [ $? -ne 0 ]; then
+		$OPT_ERR_PUSH
+		exit -3
+	fi
 fi
 removeFiles
+$OPT_EXIT_SCRIPT
